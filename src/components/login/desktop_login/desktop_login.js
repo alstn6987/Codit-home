0af2649474db24df134d547e329065203976e6f9
alstn6987 <alstn6987@gmail.com>
2021-04-12 11:51:29 +0900

    백엔드 안됨...

--- src/components/login/desktop_login/desktop_login.js
+++ src/components/login/desktop_login/desktop_login.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import codit_logo from "../../../iamges/logo-codit-search@2x.png";
 import codit_footer_logo from "../../../iamges/icon-top-logo.svg";
 import "./desktop_login.scss";
+import axios from "axios";
 
 const DesktopLogin = () => {
+  const [posts, setPosts] = useState("1");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(posts);
+    try {
+      axios.post("http://localhost:4000/events").then((res) => {
+        setPosts(res.data.id);
+        console.log(posts);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="DesktopLogin">
-      <img src={codit_logo} alt="codit-logo" />
-      <input type="email"></input>
-      <input type="password"></input>
-      <div>
-        <span>이메일 저장하기</span>
-        <span>비밀번호를 잊으셨나요?</span>
-      </div>
-      <button>로그인</button>
-      <div>
-        코딧 서치에 처음 오셨나요? <Link>회원가입</Link>
-      </div>
-      <div>
-        <img src={codit_footer_logo} alt="codit_footer_logo" />
-        <span></span>
+      <div className="container">
+        <img className="bigLogo" src={codit_logo} alt="codit-logo" />
+        <form onSubmit={onSubmit}>
+          <input type="email"></input>
+          <input type="password"></input>
+          <div className="chackBoxSection">
+            <span>이메일 저장하기</span>
+            <span>비밀번호를 잊으셨나요?</span>
+          </div>
+          <button className="logIn">로그인</button>
+        </form>
+        <div className="joinSection">
+          코딧 서치에 처음 오셨나요? <Link>회원가입</Link>
+        </div>
+        <div>
+          <img src={codit_footer_logo} alt="codit_footer_logo" />
+          <span>Copyright c 2020 CODIT Corp.All right Reserved</span>
+        </div>
       </div>
     </div>
   );
